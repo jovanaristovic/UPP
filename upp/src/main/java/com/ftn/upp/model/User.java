54bb64a234a82110327c3d6f716573fd9be6b80c
jovanaristovic <44649081+jovanaristovic@users.noreply.github.com>
2020-01-03 02:10:05 +0100

    Add files via upload

---
+++ upp/src/main/java/com/ftn/upp/model/User.java
@@ -0,0 +1,74 @@
+package com.ftn.upp.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
+    public User(Long id, String email, String password, String name, String lastName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
